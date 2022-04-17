d385a962677cad3bddf89d9626b70446dfe2ebe1
Dylan Chen <dylancfe15@gmail.com>
2022-04-17 15:51:19 -0400

    Initial Commit

---
+++ Today's History/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today's History
+//
+//  Created by Difeng Chen on 4/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
